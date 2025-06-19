531da898399563c9a5867f8db2d1e38c4119b004
NirZamir1 <nirozamir@gmail.com>
2025-06-19 18:45:56 +0300

    initial commit

---
+++ chatlib.py
@@ -0,0 +1,79 @@
+# Protocol Constants
+
+CMD_FIELD_LENGTH = 16	# Exact length of cmd field (in bytes)
+LENGTH_FIELD_LENGTH = 4   # Exact length of length field (in bytes)
+MAX_DATA_LENGTH = 10**LENGTH_FIELD_LENGTH-1  # Max size of data field according to protocol
+MSG_HEADER_LENGTH = CMD_FIELD_LENGTH + 1 + LENGTH_FIELD_LENGTH + 1  # Exact size of header (CMD+LENGTH fields)
+MAX_MSG_LENGTH = MSG_HEADER_LENGTH + MAX_DATA_LENGTH  # Max size of total message
+DELIMITER = "|"  # Delimiter character in protocol
+DATA_DELIMITER = "#"  # Delimiter in the data part of the message
+
+# Protocol Messages 
+# In this dictionary we will have all the client and server command names
+
+PROTOCOL_CLIENT = {
+"login_msg" : "LOGIN",
+"logout_msg" : "LOGOUT"
+} # .. Add more commands if needed
+
+
+PROTOCOL_SERVER = {
+"login_ok_msg" : "LOGIN_OK",
+"login_failed_msg" : "ERROR"
+} # ..  Add more commands if needed
+
+
+# Other constants
+
+ERROR_RETURN = None  # What is returned in case of an error
+
+
+def build_message(cmd, data):
+	"""
+	Gets command name (str) and data field (str) and creates a valid protocol message
+	Returns: str, or None if error occured
+	"""
+	# Implement code ...
+	command = f"{cmd}" + " " * (CMD_FIELD_LENGTH - len(cmd))
+	length = str(len(data)).zfill(LENGTH_FIELD_LENGTH)
+	return f"{command}{DELIMITER}{length}{DELIMITER}{data}" if len(data) <= MAX_DATA_LENGTH and len(cmd) <= CMD_FIELD_LENGTH else None
+
+
+def parse_message(data: str):
+	"""
+	Parses protocol message and returns command name and data field
+	Returns: cmd (str), data (str). If some error occured, returns None, None
+	"""
+	arr = data.split(DELIMITER)
+
+	if len(arr) != 3:
+		return None, None
+	[cmd, length_str, msg] = arr
+	if(len(cmd) != CMD_FIELD_LENGTH or len(length_str) != LENGTH_FIELD_LENGTH or not length_str.isdigit() or len(msg) !=int(length_str)):
+		return None, None
+	for i in range(0, LENGTH_FIELD_LENGTH):
+		if ord(length_str[i]) not in range(48,58):
+			return None, None
+	return cmd.strip(), msg
+
+	
+def split_data(msg: str, expected_fields):
+	"""
+	Helper method. gets a string and number of expected fields in it. Splits the string 
+	using protocol's data field delimiter (|#) and validates that there are correct number of fields.
+	Returns: list of fields if all ok. If some error occured, returns None
+	"""
+	# Implement code ...
+	data =msg.split("#")
+	if len(msg) != expected_fields:
+		return [None]
+	return data
+
+
+def join_data(msg_fields):
+	"""
+	Helper method. Gets a list, joins all of it's fields to one string divided by the data delimiter. 
+	Returns: string that looks like cell1#cell2#cell3
+	"""
+	return "#".join(msg_fields)
+
---
+++ client.py
@@ -0,0 +1,92 @@
+import socket
+import chatlib  # To use chatlib functions or consts, use chatlib.****
+
+SERVER_IP = "127.0.0.1"  # Our server will run on same computer as client
+SERVER_PORT = 5678
+
+# HELPER SOCKET METHODS
+
+
+def build_and_send_message(conn: socket.socket, code, data):
+    """
+    Builds a new message using chatlib, wanted code and message. 
+    Prints debug info, then sends it to the given socket.
+    Paramaters: conn (socket object), code (str), data (str)
+    Returns: Nothing
+    """
+    message = chatlib.build_message(code, data)
+    print(f"Sending message: {message}")
+    conn.sendall(message.encode('utf-8'))
+
+
+def recv_message_and_parse(conn: socket.socket):
+    """
+    Recieves a new message from given socket,
+    then parses the message using chatlib.
+    Paramaters: conn (socket object)
+    Returns: cmd (str) and data (str) of the received message. 
+    If error occured, will return None, None
+    """
+    # Implement Code
+    # ..
+    try:
+        data = ""
+        while True:
+            new_data = conn.recv(1024).decode('utf-8')
+            if not new_data:
+                break
+            data += new_data
+
+        cmd, data = chatlib.parse_message(data)
+    except Exception as e:
+        return None, None
+    return cmd, data
+
+
+def connect():
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        soc.connect((SERVER_IP, SERVER_PORT))
+
+    except socket.error as e:
+        error_and_exit(f"Error connecting to server: {e}")
+    finally:
+        return soc
+
+
+def error_and_exit(error_msg):
+    """Prints an error message and exits the program."""
+    raise Exception(error_msg)
+
+
+def login(conn):
+    if not conn:
+        error_and_exit("Connection is not established. Cannot login.")
+    while True:
+        username = input("Please enter username: \n")
+        password = input("Please enter password: \n")
+        build_and_send_message(
+            conn, chatlib.PROTOCOL_CLIENT["login_msg"], f"{username}#{password}")
+        cmd, data = recv_message_and_parse(conn)
+        if cmd == chatlib.PROTOCOL_SERVER["login_ok_msg"]:
+            print("Login successful!")
+            return
+        elif cmd == chatlib.PROTOCOL_SERVER["login_failed_msg"]:
+            print("Login failed. Please try again.")
+        else:
+            error_and_exit(f"Unexpected response from server: {cmd}, {data}")
+
+
+def logout(conn):
+    build_and_send_message(conn, chatlib.PROTOCOL_CLIENT["logout_msg"], "")
+
+
+def main():
+    socket_conn = connect()
+    login(socket_conn)
+    logout(socket_conn)
+    socket_conn.close()
+
+
+if __name__ == '__main__':
+    main()
